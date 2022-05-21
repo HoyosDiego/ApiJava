b0789e2829f9c0c45c98ecb84dc0dc473e6cd084
hoyosDiego29 <94981003+hoyosDiego29@users.noreply.github.com>
2022-05-20 23:46:16 -0500

    Api Java

---
+++ src/main/java/com/example/demo/controllers/UsuarioController.java
@@ -0,0 +1,52 @@
+package com.example.demo.controllers;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import com.example.demo.models.UsuarioModel;
+import com.example.demo.services.usuarioService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {
+    @Autowired
+    usuarioService usuarioService;
+
+    @GetMapping()
+    public ArrayList<UsuarioModel> obtenerUsuarios() {
+
+        return usuarioService.obtenerUsuarios();
+    }
+
+    @PostMapping()
+    public UsuarioModel guardarUsuario(@RequestBody UsuarioModel usuario) {
+        return this.usuarioService.guardarUsuario(usuario);
+    }
+   
+      @GetMapping(path = "/{id}")
+      public Optional<UsuarioModel> obtnerUsuarioPorId(@PathVariable("id") Long id)
+      {
+      return this.usuarioService.obtenerPorId(id);
+      }
+      
+       /*
+     * @GetMapping(path = "/query")
+     * public ArrayList<UsuarioModel>
+     * obtnerUsuarioPorPrioridad(@RequestParam("prioridad") Integer prioridad) {
+     * return this.usuarioService.obtnerPorPrioridad(prioridad);
+     * }
+     */
+      @DeleteMapping( path = "/{id}")
+      public String eliminarPorId(@PathVariable("id") Long id) {
+      boolean ok = this.usuarioService.eliminarUsuario(id);
+      if (ok) {
+      return "Se elimino el usuario con id " + id;
+      } else {
+      return "No se pudo eliminar usuario con id " + id;
+      }
+      }
+     
+}
---
+++ src/main/java/com/example/demo/models/UsuarioModel.java
@@ -0,0 +1,40 @@
+package com.example.demo.models;
+import java.io.Serializable;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "usuario")
+
+public class UsuarioModel implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+    private String nombre;
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
